564f28c2db9c5c4eec29169738815486d508106f
cpow <cpow85@gmail.com>
2013-05-21 10:42:53 -0400

    initial commit with the twitter api stuff set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashtag::Application.config.secret_token = 'cae131b5c7c6aa363733ed3a15df425598448b2187915d415661e70f010c5224c7e2fd62e7b51f0e88169c68d6a6c936c1bf7880eadbd5ae9dc8dcb4c0235748'
